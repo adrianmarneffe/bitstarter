071c8102647f514baa28383c38978624da81ef34
Ad <example@stanford.edu>
2013-07-11 13:40:32 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,8 +1,8 @@
 var express = require('express');
-var fs = require "fs");
+var fs = require ("fs");
 var app = express.createServer(express.logger());
 var page = fs.readFileSync (index2.html);
-var htmlpage = buf.toString (page)
+var htmlpage = buf.toString (page);
 app.get('/', function(request, response) {
   response.send (htmlpage);
 });

b2ac31f6b1a77bf61aa67dd5b69c7670ac6dced1
Ad <example@stanford.edu>
2013-07-11 11:43:09 +0000

    added updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 var express = require('express');
-
+var fs = require "fs");
 var app = express.createServer(express.logger());
-
+var page = fs.readFileSync ('./index2.html' , 'utf8')
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send (page);
 });
 
 var port = process.env.PORT || 5000;

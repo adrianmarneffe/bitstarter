06a60569a834f97aca2db5bdd092c92f7d4d4513
Ad <example@stanford.edu>
2013-07-12 08:41:04 +0000

    new day new try

--- web.js
+++ web.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var fs = require ('fs');
 var app = express.createServer(express.logger());
-
+var http = require ('http');
 
 var page = fs.readFileSync('./index2.html');
 
 
 
 app.get('/', function(request, response) {
-  response.send (page);
+response.writeHead (200, 'Content-Type': 'text/plain'); 
+   response.write (page);
 });
 
 var port = process.env.PORT || 5000;
